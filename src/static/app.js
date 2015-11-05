bfdd84a21450be3a5986a45c3f24b7db306c7131
Markus Unterwaditzer <markus@unterwaditzer.net>
2015-11-05 19:03:50 +0100

    Restyling

---
+++ src/static/app.js
@@ -0,0 +1,21 @@
+(function() {
+    var elements = document.getElementsByClassName("user-agent");
+    $(".user-agent").html(function() {
+        var parser = new UAParser($("").text());
+        var ua = parser.getResult();
+
+        var newHTML = "";
+        newHTML +=
+            '<img title="' +
+            ua.browser.name +
+            '" src="/static/ua/browser/' +
+            encodeURI(ua.browser.name.toLowerCase()) +
+            '.png" /> ';
+        newHTML += '<img title="' +
+            ua.os.name +
+            '" src="/static/ua/os/' +
+            encodeURI(ua.os.name.toLowerCase()) +
+            '.png" />';
+        return newHTML;
+    });
+})()
