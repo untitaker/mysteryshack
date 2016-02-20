801bde4c09c5f63718d56213052a762efff22797
Markus Unterwaditzer <markus@unterwaditzer.net>
2016-02-20 19:45:12 +0100

    Remove useless comment

--- src/mysteryshack/lib.rs
+++ src/mysteryshack/lib.rs
@@ -2,7 +2,6 @@
 #![cfg_attr(feature = "clippy", feature(plugin))]
 #![cfg_attr(feature = "clippy", plugin(clippy))]
 #![cfg_attr(feature = "clippy", deny(warnings))]
-//#![cfg_attr(feature = "clippy", allow(needless_lifetimes))]
 
 extern crate rustc_serialize;
 #[macro_use] extern crate iron;
