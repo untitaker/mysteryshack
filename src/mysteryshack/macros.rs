ec5165e03151039e031bde01b9e24eaf8642abe5
Markus Unterwaditzer <markus@unterwaditzer.net>
2016-02-24 00:22:46 +0100

    Add JSON literal macro

---
+++ src/mysteryshack/macros.rs
@@ -0,0 +1,13 @@
+macro_rules! json {
+  (null) => (::rustc_serialize::json::Json::Null);
+  {$($keys:expr => $values:expr),*} => ({
+      use ::rustc_serialize::json::Json;
+      let kv_pairs = vec![ $(($keys.to_string(), json!($values))),* ];
+      Json::Object(kv_pairs.into_iter().collect())
+  });
+  ($value:expr) => ({
+      use ::rustc_serialize::json::ToJson;
+      $value.to_json()
+  });
+  [[ $($values:expr),* ]] => (::rustc_serialize::json::Json::Array(vec![ $(json!($values)),* ]));
+}
